29389ee9782c59ca2bd8812be84b535013a2dc43
SeongbinPark <binni0515@gmail.com>
2022-08-20 16:05:44 +0900

    MemberRepository, MemberService

---
+++ src/main/java/jpabook/myjpashop/service/MemberService.java
@@ -0,0 +1,46 @@
+package jpabook.myjpashop.service;
+
+import jpabook.myjpashop.domain.Member;
+import jpabook.myjpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> members = memberRepository.findByName(member.getName());
+        if (!members.isEmpty()) {
+            throw new IllegalStateException("이미 있는 회원");
+        }
+    }
+
+    public Member findById(Long memberId) {
+        return memberRepository.find(memberId);
+    }
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    public List<Member> findByName(String name) {
+        return memberRepository.findByName(name);
+    }
+
+
+
+}
