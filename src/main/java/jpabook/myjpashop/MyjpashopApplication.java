be0e90ac96e0b1ae76717590692b44779f976fc4
SeongbinPark <binni0515@gmail.com>
2022-08-21 11:38:03 +0900

    Add MemberController

--- src/main/java/jpabook/myjpashop/MyjpashopApplication.java
+++ src/main/java/jpabook/myjpashop/MyjpashopApplication.java
@@ -3,14 +3,10 @@ package jpabook.myjpashop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 @SpringBootApplication
 public class MyjpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyjpashopApplication.class, args);
 	}
-
 }
